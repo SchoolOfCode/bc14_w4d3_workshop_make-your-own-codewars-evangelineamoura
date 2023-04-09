d1e085a54de36b19edfa59adf7712f4a5517f632
Evangeline Amoura <evangelineamoura@yahoo.com>
2023-04-09 23:41:10 +0100

    kata and tests completed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 👉 Write your kata here!
 Oh no! Rupunzilla is trapped in a tower! She's not going to let you climb up her hair because she quite likes keeping it on her head, so you are going to have to try something else if you want to save her! 
 
-Luckily for you the witch that trapped her kept a list of all the people and things she has in the various towers under her control. You must enter the magical code that frees Rupunzilla. Make sure that you return the phrase "I found Rupunzilla in tower ..." and enter the index where you found her. 
+Luckily for you the witch that trapped her kept a list of all the people and things she has in the various towers under her control. You must enter the magical code that frees Rupunzilla. Make sure that you return the phrase "I found Rupunzilla in tower " and enter the index where you found her. 
 
 e.g:
 ["stockpile of cheese", "winter socks", "Brianna", "5 tiny elves", "that one shopkeeper who wouldn't sell me any bananas because she 'didn't have any!'", "Rupunzilla", "a bunch of paperclips", "pointy shoes"] 
@@ -12,6 +12,9 @@ Would return "I found Rupunzilla in tower 5"
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
-function whereArtThou(illGottenGains) {
+
+
+export function whereArtThou(illGottenGains) {
     //enter code here!
-}
+    
+};
--- main.test.js
+++ main.test.js
@@ -1,1 +1,39 @@
 //👉 Write your tests below here:
+
+import {test, expect} from "@jest/globals";
+import {whereArtThou} from "./main.js";
+
+// Should go through a selection of arrays and find Rupunzilla in each of them
+// Rupunzilla should be at a different index in each array
+// Should return a string of "I found Rupunzilla in tower " + index 
+// 
+
+test ('find Rupunzilla in an array', function(){
+       
+        const illGottenGains = ["stockpile of cheese", "winter socks", "Brianna", "5 tiny elves", "that one shopkeeper who wouldn't sell me any bananas because she 'didn't have any!'", "Rupunzilla", "a bunch of paperclips", "pointy shoes"]; 
+        
+        const expected = ("I found Rupunzilla in tower 5");
+        
+        expect(whereArtThou(illGottenGains)).toBe(expected);
+        
+    })
+
+    test ('find Rupunzilla in an array', function(){
+       
+        const illGottenGains = ["stockpile of cheese", "Rupunzilla", "a large collection of mismatched left foot toe-socks", "3 ducks wearing top hats", "garlic bread", "pile of rubies", "a bunch of paperclips", "pointy shoes"]; 
+        
+        const expected = ("I found Rupunzilla in tower 1");
+        
+        expect(whereArtThou(illGottenGains)).toBe(expected);
+        
+    })   
+
+ test ('find Rupunzilla in an array of numbers', function(){
+       
+        const illGottenGains = [79, 85, 2, "Rupunzilla", 9837465, 100000, 28, 11, 30, 7]; 
+        
+        const expected = ("I found Rupunzilla in tower 3");
+        
+        expect(whereArtThou(illGottenGains)).toBe(expected);
+        
+    })   
